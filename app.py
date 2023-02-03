c5dc628d5437c470f90b11a6a95c78a63a3b631e
rodriV <utndavid@gmail.com>
2023-02-02 21:26:22 -0300

    Carga templates

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
-
 
 @app.route('/')
 def home():
     
-    return 'Mi primer proyecto Flask'
+    return render_template('home-banking.html')
 
 
 if __name__ == '__main__':
