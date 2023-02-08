9fd2bff96e464b17a05521dbf33faf97a1b5f4e3
rodriV <utndavid@gmail.com>
2023-02-07 21:24:17 -0300

    Crea cuentas desde archivo csv

--- app.py
+++ app.py
@@ -4,10 +4,14 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    
-    return render_template('home-banking.html')
+    persona_titular = lista_de_datos['dni']
+
+    return render_template('home-banking.html',saludo=persona_titular.saludo(),movements=persona_titular.obtener_todos_los_movimientos())
 
 
 if __name__ == '__main__':
-    
+    import proceso_cuentas
+
+    lista_de_datos = proceso_cuentas.crear_cuentas()
+
     app.run(debug=True)
